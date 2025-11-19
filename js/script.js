960ec2793010b3e0b2cee39f25ee7c84e25edab2
mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-11-19 13:42:06 +0200

    final touch

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-var imagesList = Array.from(document.querySelectorAll(".row img"))
+var imagesList = Array.from(document.querySelectorAll(".row img")) // array of all images
 var popUpDiv = document.querySelector(".popUp")
 var xMark = document.querySelector(".close")
 var left = document.querySelector(".left")
 var right = document.querySelector(".right")
-var imageIndex;
+var imageIndex;//variable to put clicked image index in
 var wideImage = document.querySelector(".imgSlider")
+// for loop to show the slider if any image click and show it in the slide and put clicked image index in variable imageIndex
 for (let i = 0; i < imagesList.length; i++) {
     imagesList[i].addEventListener("click", function (e) {
         imageIndex = imagesList.indexOf(e.target)
@@ -13,24 +14,36 @@ for (let i = 0; i < imagesList.length; i++) {
         wideImage.style.backgroundImage = `url(${currentSrc})`
     })
 }
-document.addEventListener("keyup",function(e){
-    if(e.key=="ArrowRight"){
-        slide(1)
-    }else if(e.key=="ArrowLeft"){
-        slide(-1)
-    }else if(e.key=="Escape"){
+// to close the slider if i clicked the popUpDiv
+document.addEventListener("click", function (e) {
+    if (e.target == popUpDiv) {
         close()
     }
 })
+// one event to handle if i click arrow right, arrow left or esc
+document.addEventListener("keyup", function (e) {
+    if (e.key == "ArrowRight") {
+        slide(1)
+    } else if (e.key == "ArrowLeft") {
+        slide(-1)
+    } else if (e.key == "Escape") {
+        close()
+    }
+})
+// event to handle if i click left arrow icon
 left.addEventListener("click", function () {
     slide(-1)
 })
+// event to handle if i click right arrow icon
 right.addEventListener("click", function () {
     slide(1)
 })
+// event to handle if i click close arrow icon
 xMark.addEventListener("click", function () {
-        close()
-    })
+    close()
+})
+// fun to has the logic of prev or next based on parameter step if -1 then prev
+// if 1 then next
 function slide(step) {
     imageIndex = imageIndex + step
     if (imageIndex < 0) {
@@ -41,7 +54,8 @@ function slide(step) {
     const nextScr = imagesList[imageIndex].getAttribute("src")
     wideImage.style.backgroundImage = `url(${nextScr})`
 }
+// fun to has logic of close icon
 function close() {
-        popUpDiv.classList.replace("d-flex", "d-none")
+    popUpDiv.classList.replace("d-flex", "d-none")
 }
 
